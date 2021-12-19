64743985645cad567d6b8406e4fcd5b96e2a8d41
devbotas <simon@devbot.lt>
2021-12-19 18:40:01 +0200

    Adding a simple demo with motor and screen.

--- Vela/Program.cs
+++ Vela/Program.cs
@@ -2,6 +2,9 @@
 
 namespace Vela {
     public class Program {
+        private static BrickletLCD128x64 _brickletLCD128X64;
+        private static BrickSilentStepper _brickSilentStepper;
+
         public static void Main(string[] args) {
 
             // Running Tinkerforge daemon from within the container. This is a workaround until proper Dockerfiles are constructed.
@@ -9,13 +12,22 @@ namespace Vela {
             Thread.Sleep(5000);
 
             var brickIp = "localhost";
-            LoadEnvOrDie(brickIp, "localhost");
+            //var brickIp = "172.16.0.199";
+            // LoadEnvOrDie(brickIp, "localhost");
             var isConnected = false;
 
             IPConnection _brickConnection;
             _brickConnection = new IPConnection();
             _brickConnection.EnumerateCallback += (IPConnection sender, string UID, string connectedUID, char position, short[] hardwareVersion, short[] firmwareVersion, int deviceIdentifier, short enumerationType) => {
                 Console.WriteLine($"Enumerated: {UID}");
+
+                if (deviceIdentifier == BrickletLCD128x64.DEVICE_IDENTIFIER) {
+                    _brickletLCD128X64 = new BrickletLCD128x64(UID, sender);
+                }
+
+                if (deviceIdentifier == BrickSilentStepper.DEVICE_IDENTIFIER) {
+                    _brickSilentStepper = new BrickSilentStepper(UID, sender);
+                }
             };
 
             _brickConnection.Connected += (IPConnection sender, short connectReason) => {
@@ -36,6 +48,26 @@ namespace Vela {
                     Thread.Sleep(5000);
                 }
             }
+
+            while ((_brickletLCD128X64 == null) && (_brickSilentStepper == null)) {
+                Thread.Sleep(100);
+            }
+
+            _brickSilentStepper.Enable();
+            _brickSilentStepper.SetSpeedRamping(5000, 5000);
+            _brickSilentStepper.SetMaxVelocity(10000);
+            _brickSilentStepper.SetStepConfiguration(BrickSilentStepper.STEP_RESOLUTION_16, false);
+            _brickletLCD128X64.ClearDisplay();
+            _brickletLCD128X64.RemoveAllGUI();
+            var position = 0;
+            _brickletLCD128X64.GUIButtonPressedCallback += (BrickletLCD128x64 sender, byte index, bool pressed) => {
+                position += 5000;
+                _brickSilentStepper.SetTargetPosition(position);
+            };
+
+            _brickletLCD128X64.SetGUIButton(0, 34, 20, 60, 20, "Forward!");
+
+            _brickletLCD128X64.SetGUIButtonPressedCallbackConfiguration(100, true);
 
             Thread.Sleep(-1);
         }
